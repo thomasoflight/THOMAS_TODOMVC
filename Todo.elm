19f7d9355b5a37adf8754e559c9ce2d4b492b04e
A.M. Thomas <thomasoflight@lightfaced.org>
2018-01-12 10:48:31 -0500

    0.2

--- Todo.elm
+++ Todo.elm
@@ -1,8 +1,36 @@
 module Todo exposing (..)
 
 import Html exposing (..)
+import Html.Attributes exposing (..)
+
+
+type alias Model =
+    { todos : List String
+    , data : String
+    }
+
+
+model : Model
+model =
+    { todos = [ "fake_todo_01", "fake_todo_02", "..." ]
+
+    {- right now, each todo will be considered a string,
+       no other metadata will be considered like which todo number this is..
+       or if the todo is completed
+    -}
+    , data = "..." -- this holds whatever the user will be typing into an empty task slot
+    }
+
+
+view =
+    div []
+        [ h1 [] [ text "Todo" ]
+        , div [ class "todos-box" ]
+            [ p [] [ text "our todos will go here" ]
+            ]
+        ]
 
 
 main : Html msg
 main =
-    text "Let's make a Todo app"
+    view
