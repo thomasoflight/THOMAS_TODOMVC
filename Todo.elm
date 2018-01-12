0d3948faf2868767c846063b0136bc4691019507
A.M. Thomas <thomasoflight@lightfaced.org>
2018-01-12 11:16:11 -0500

    0.3 -- viewEntries

--- Todo.elm
+++ Todo.elm
@@ -27,8 +27,37 @@ view =
         [ h1 [] [ text "Todo" ]
         , div [ class "todos-box" ]
             [ p [] [ text "our todos will go here" ]
+            , viewEntries
             ]
         ]
+
+
+viewEntries =
+    let
+        renderEntry entry =
+            li [] [ text (toString entry) ]
+    in
+        div [] (List.map renderEntry model.todos)
+
+
+
+-- THE DEAL WITH 'let / in' --
+{- Ok so anytime you see 'let' it means, "everything until the 'in' is a building block I'm going to use in the part
+    after the 'in' " Just like 'lambda' functions, 'let' gives you a temporary namespace to define variables,
+   functions etc. Compare the alternative below, which does the same thing:
+    `
+            renderEntry entry =
+                li [] [ text (toString entry) ]
+
+            viewEntries =
+                div [] (List.map renderEntry model.todos)
+
+    BUT it's kind of weird right? I mean one line per function? And they go together kind right?
+    Doesn't that maybe feel weird like it would be nice to combine them?
+    If you feel this way, Let / In is your friend. viewEntries in this case is like,
+    "hey I need a function for this other function.
+    Lemme make that first and then I can use it for the List.map which needs a function..."
+-}
 
 
 main : Html msg
