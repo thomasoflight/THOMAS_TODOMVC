43a091c2aeac4df9935532001a43ece1363405d5
A.M. Thomas <thomasoflight@lightfaced.org>
2018-03-06 01:09:38 -0500

    Styling and refactoring

--- Todo.elm
+++ Todo.elm
@@ -1,9 +1,12 @@
 module Todo exposing (..)
 
 import Dom
+import Task
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
+import Html.Keyed as Keyed
+import Html.Lazy exposing (lazy, lazy2)
 import Json.Decode as Json
 
 
@@ -30,7 +33,7 @@ init =
 
 type alias Model =
     { todoItems : List TodoItem
-    , data : String
+    , todoInputData : String
     , uidCounter : Int
     , visibility : String
     }
@@ -40,6 +43,7 @@ type alias TodoItem =
     { desc : String
     , isComplete : Bool
     , uid : Int
+    , isEditing : Bool
     }
 
 
@@ -48,6 +52,7 @@ newTodo userInput uid =
     [ { desc = userInput
       , isComplete = False
       , uid = uid
+      , isEditing = False
       }
     ]
 
@@ -73,11 +78,13 @@ type Msg
     = NoOp
     | Add
     | UpdateField String
+    | UpdateDesc Int String
     | ToggleComplete Int Bool
-    | MarkCompleteAll
+    | ToggleAllComplete
     | DeleteOneTodo Int
     | DeleteCompleted
     | ChangeVisibility String
+    | EditEntry Int Bool
 
 
 update msg model =
@@ -89,16 +96,26 @@ update msg model =
             { model
                 | uidCounter = model.uidCounter + 1
                 , todoItems =
-                    if String.isEmpty model.data then
+                    if String.isEmpty model.todoInputData then
                         model.todoItems
                     else
-                        model.todoItems ++ (newTodo model.data model.uidCounter)
-                , data = ""
+                        model.todoItems ++ (newTodo model.todoInputData model.uidCounter)
+                , todoInputData = ""
             }
                 ! []
 
         UpdateField str ->
-            { model | data = str } ! []
+            { model | todoInputData = str } ! []
+
+        UpdateDesc uid str ->
+            let
+                updateDesc t =
+                    if t.uid == uid then
+                        { t | desc = str }
+                    else
+                        t
+            in
+                { model | todoItems = (List.map updateDesc model.todoItems) } ! []
 
         ToggleComplete uid bool ->
             let
@@ -110,7 +127,7 @@ update msg model =
             in
                 { model | todoItems = List.map isCompleted model.todoItems } ! []
 
-        MarkCompleteAll ->
+        ToggleAllComplete ->
             if List.all .isComplete model.todoItems then
                 { model | todoItems = List.map (\t -> { t | isComplete = False }) model.todoItems } ! []
             else
@@ -126,84 +143,129 @@ update msg model =
         ChangeVisibility str ->
             { model | visibility = str } ! []
 
+        EditEntry uid bool ->
+            let
+                editDesc todo =
+                    if todo.uid == uid then
+                        { todo | isEditing = bool }
+                    else
+                        todo
+            in
+                { model | todoItems = List.map editDesc model.todoItems }
+                    ! [ Task.attempt (\_ -> NoOp) (Dom.focus ("todo-" ++ toString uid))
+                      ]
+
 
 view : Model -> Html Msg
 view model =
     div []
-        [ h1 [] [ text "Todo" ]
-        , div [ class "todos-box" ]
-            [ viewInput model.data
-            , viewTodos model.todoItems model.visibility
-            , footer []
-                [ button [ onClick (DeleteCompleted) ] []
-                , button [ onClick (ChangeVisibility "all") ] [ text "all" ]
-                , button [ onClick (ChangeVisibility "active") ] [ text "active" ]
-                , button [ onClick (ChangeVisibility "completed") ] [ text "completed" ]
-                , p [] [ text (model.visibility) ]
+        -- class "todomvc-wrapper" we don't have a sidebar on our cloned
+        -- so it's probably ok to leave this out
+        [ section []
+            [ div
+                [ class "todos-box"
                 ]
+                {- we moved h1 element and placed into a subsection below.
+                   Since the main view function is the highest level, we want to keep
+                   more detailed stuff in separate functions below. The main view
+                   is just putting it all together for us.
+                -}
+                [ lazy2 renderMainInput model.todoInputData model.todoItems
+                , lazy2 renderTodoItems model.todoItems model.visibility
+                , lazy renderFilters model
+                ]
+            , infoFooter
             ]
         ]
 
 
-viewTodos : List TodoItem -> String -> Html Msg
-viewTodos todoItems visibility =
-    let
-        renderEntry todo =
-            div
-                [ case visibility of
-                    "active" ->
-                        if todo.isComplete then
-                            class "hidden"
-                        else
-                            class "active"
-
-                    "completed" ->
-                        if (not todo.isComplete) then
-                            class "hidden"
-                        else
-                            class "active"
-
-                    _ ->
-                        class "active"
-                ]
-                [ li
-                    []
-                    [ input
-                        [ type_ "checkbox"
-                        , checked todo.isComplete
-                        , onClick (ToggleComplete todo.uid (not todo.isComplete))
-                        ]
-                        []
-                    , label [] [ text (todo.desc) ]
-                    , button [ class "delete-one-todo", onClick (DeleteOneTodo todo.uid) ] []
-                    ]
-                ]
-    in
-        ul [ class "todo-items" ]
-            (List.map
-                renderEntry
-                todoItems
-            )
-
-
-viewInput : String -> Html Msg
-viewInput data =
-    div []
-        [ input
-            [ type_ "checkbox"
-            , checked False
-            , onClick MarkCompleteAll
-            ]
-            []
+renderMainInput : String -> List TodoItem -> Html Msg
+renderMainInput todoInputData todoItems =
+    -- we moved allComplete to a new section
+    -- moving from general div to more semantic html tags e.g. section/header/footer...
+    header []
+        [ h1 [] [ text "Todo" ]
         , input
             [ class "todo-insert-new"
-            , type_ "text"
             , placeholder "What needs to be done?"
             , autofocus True
-            , value data
+            , value todoInputData
             , name "newTodo"
             , onInput UpdateField
             , onEnter Add
             ]
             []
+        ]
+
+
+renderTodoItems : List TodoItem -> String -> Html Msg
+renderTodoItems todoItems visibility =
+    let
+        forRender =
+            case visibility of
+                "active" ->
+                    List.filter (\a -> a.isComplete /= True) todoItems
+
+                "completed" ->
+                    List.filter .isComplete todoItems
+
+                _ ->
+                    todoItems
+
+        allComplete =
+            List.all .isComplete todoItems && (not (List.isEmpty todoItems))
+    in
+        section []
+            [ input
+                [ type_ "checkbox"
+                , checked allComplete
+                , onClick ToggleAllComplete
+                ]
+                []
+            , label [ for "toggle-all" ] [ text "Mark all as complete" ]
+            , ul [ class "todo-items" ] <| List.map renderOneTodo forRender
+            ]
+
+
+renderOneTodo : TodoItem -> Html Msg
+renderOneTodo todo =
+    li
+        [ classList [ ( "editing", todo.isEditing ) ] ]
+        [ div
+            [ class "view" ]
+            [ input
+                [ type_ "checkbox"
+                , checked todo.isComplete
+                , onClick (ToggleComplete todo.uid (not todo.isComplete))
+                ]
+                []
+            , label [ onDoubleClick (EditEntry todo.uid True) ] [ text todo.desc ]
+            , button [ class "delete-one-todo", onClick (DeleteOneTodo todo.uid) ] [ text "x" ]
+            ]
+        , input
+            [ class "edit"
+            , value todo.desc
+            , name "title"
+            , id ("todo-" ++ toString todo.uid)
+            , onInput (UpdateDesc todo.uid)
+            , onBlur (EditEntry todo.uid False)
+            , onEnter (EditEntry todo.uid False)
+            ]
+            [ text (todo.desc) ]
+        ]
+
+
+renderFilters model =
+    div []
+        [ button [ onClick (DeleteCompleted) ] [ text <| toString <| List.length model.todoItems ]
+        , button [ onClick (ChangeVisibility "all") ] [ text "all" ]
+        , button [ onClick (ChangeVisibility "active") ] [ text "active" ]
+        , button [ onClick (ChangeVisibility "completed") ] [ text "completed" ]
+        ]
+
+
+infoFooter : Html msg
+infoFooter =
+    footer []
+        [ p [] [ text "Some basic footer info" ]
         ]
